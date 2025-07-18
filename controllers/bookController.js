9d60042ce72d41b8ab549518441608653215a4f6
240129745 <240129745@qq.com>
2025-07-18 13:41:25 +0800

    update books

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -6,11 +6,11 @@ const {
     body,
     validationResult
 } = require("express-validator");
-//const { sanitizeBody } = require("express-validator");
+//const { body } = require("express-validator");
 
 const asyncHandler = require("express-async-handler");
 
-exports.index = asyncHandler(async(req, res, next) => {
+exports.index = asyncHandler(async (req, res, next) => {
     // 并行获取书的详细信息、书实例、作者和体裁的数量
     const [
         numBooks,
@@ -19,14 +19,14 @@ exports.index = asyncHandler(async(req, res, next) => {
         numAuthors,
         numGenres,
     ] = await Promise.all([
-                Book.countDocuments({}).exec(),
-                BookInstance.countDocuments({}).exec(),
-                BookInstance.countDocuments({
-                    status: "Available"
-                }).exec(),
-                Author.countDocuments({}).exec(),
-                Genre.countDocuments({}).exec(),
-            ]);
+        Book.countDocuments({}).exec(),
+        BookInstance.countDocuments({}).exec(),
+        BookInstance.countDocuments({
+            status: "Available"
+        }).exec(),
+        Author.countDocuments({}).exec(),
+        Genre.countDocuments({}).exec(),
+    ]);
 
     res.render("index", {
         title: "玉林图书馆",
@@ -40,7 +40,7 @@ exports.index = asyncHandler(async(req, res, next) => {
 });
 
 // 呈现数据库中所有书本的列表
-exports.book_list = asyncHandler(async(req, res, next) => {
+exports.book_list = asyncHandler(async (req, res, next) => {
     const allBooks = await Book.find({}, "title author")
         .sort({
             title: 1
@@ -55,14 +55,14 @@ exports.book_list = asyncHandler(async(req, res, next) => {
 });
 
 // 显示特定书籍的详细信息页面。
-exports.book_detail = asyncHandler(async(req, res, next) => {
+exports.book_detail = asyncHandler(async (req, res, next) => {
     // 获取书籍的详细信息，以及特定书籍的实例
     const [book, bookInstances] = await Promise.all([
-                Book.findById(req.params.id).populate("author").populate("genre").exec(),
-                BookInstance.find({
-                    book: req.params.id
-                }).exec(),
-            ]);
+        Book.findById(req.params.id).populate("author").populate("genre").exec(),
+        BookInstance.find({
+            book: req.params.id
+        }).exec(),
+    ]);
 
     if (book === null) {
         // 没有结果。
@@ -153,24 +153,24 @@ exports.book_create_post = [
                 },
             },
                 function (err, results) {
-                if (err) {
-                    return next(err);
-                }
-
-                // Mark our selected genres as checked.
-                for (let i = 0; i < results.genres.length; i++) {
-                    if (book.genre.indexOf(results.genres[i]._id) > -1) {
-                        results.genres[i].checked = "true";
+                    if (err) {
+                        return next(err);
                     }
-                }
-                res.render("book_form", {
-                    title: "Create Book",
-                    authors: results.authors,
-                    genres: results.genres,
-                    book: book,
-                    errors: errors.array(),
-                });
-            }, );
+
+                    // Mark our selected genres as checked.
+                    for (let i = 0; i < results.genres.length; i++) {
+                        if (book.genre.indexOf(results.genres[i]._id) > -1) {
+                            results.genres[i].checked = "true";
+                        }
+                    }
+                    res.render("book_form", {
+                        title: "Create Book",
+                        authors: results.authors,
+                        genres: results.genres,
+                        book: book,
+                        errors: errors.array(),
+                    });
+                },);
             return;
         } else {
             // Data from form is valid. Save book.
@@ -183,17 +183,17 @@ exports.book_create_post = [
 ];
 
 // 通过 GET 显示删除图书。
-exports.book_delete_get = asyncHandler(async(req, res, next) => {
+exports.book_delete_get = asyncHandler(async (req, res, next) => {
     res.send("未实现：删除 GET");
 });
 
 // 以 POST 方式处理删除图书。
-exports.book_delete_post = asyncHandler(async(req, res, next) => {
+exports.book_delete_post = asyncHandler(async (req, res, next) => {
     res.send("未实现：删除 POST");
 });
 
 // 通过 GET 显示更新图书。
-exports.book_update_get = asyncHandler(async(req, res, next) => {
+exports.book_update_get = asyncHandler(async (req, res, next) => {
     //res.send("未实现：更新图书 GET");
     try {
         let book = await Book.findById(req.params.id).populate("author").populate("genre");
@@ -204,7 +204,7 @@ exports.book_update_get = asyncHandler(async(req, res, next) => {
         }
 
         for (let i = 0; i < genres.length; i++) {
-            for (let j = 0; j < book.genre.length;j++) {
+            for (let j = 0; j < book.genre.length; j++) {
                 if (
                     genres[i]._id.toString() == book.genre[j]._id.toString()) {
                     genres[i].checked = "true";
@@ -224,6 +224,28 @@ exports.book_update_get = asyncHandler(async(req, res, next) => {
 });
 
 // 处理 POST 时的更新图书。
-exports.book_update_post = asyncHandler(async(req, res, next) => {
-    res.send("未实现：更新图书 POST");
-});
+exports.book_update_post = [
+    // Convert the genre to an array.
+    (req, res, next) => {
+        if (!(req.body.genre instanceof Array)) {
+            if (typeof req.body.genre === "undefined")
+                req.body.genre = [];
+            else
+                req.body.genre = new Array(req.body.genre);
+        }
+        next();
+    },
+      body("title", "Title must not be empty.").isLength({ min: 1 }).trim(),
+  body("author", "Author must not be empty.").isLength({ min: 1 }).trim(),
+  body("summary", "Summary must not be empty.").isLength({ min: 1 }).trim(),
+  body("isbn", "ISBN must not be empty").isLength({ min: 1 }).trim(),
+
+  // Sanitize fields.
+  body("title").trim().escape(),
+  body("author").trim().escape(),
+  body("summary").trim().escape(),
+  body("isbn").trim().escape(),
+  body("genre.*").trim().escape(),
+
+
+]
